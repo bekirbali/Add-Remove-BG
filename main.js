01f9e1cd23798de6029f875951c7086988620ab4
bekirbali <bfbali43@gmail.com>
2023-01-13 18:52:47 +0300

    keep changing bgc

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const body = document.querySelector("body");
 console.log(body);
 
-let c1 = Math.round(Math.random() * 255);
-let c2 = Math.round(Math.random() * 255);
-let c3 = Math.round(Math.random() * 255);
-
 const button = document.querySelector(".change-color");
-
 button.addEventListener("click", (e) => {
-  body.style.backgroundColor += `rgb(${c1},${c2},${c3})`;
+  let c1 = Math.round(Math.random() * 255);
+  let c2 = Math.round(Math.random() * 255);
+  let c3 = Math.round(Math.random() * 255);
+  let rgb = `rgb(${c1},${c2},${c3})`;
+  if (e) {
+    body.style.backgroundColor = rgb;
+  }
+
+  console.log(e);
 });
